8f4502a07d535cab5bfb898fc4fb05bfec4ce840
hsidd1 <hamzasiddiqui247@gmail.com>
2023-06-08 21:00:29 -0400

    restructure radar frame class

--- Radar_frame.py
+++ Radar_frame.py
@@ -1,23 +1,49 @@
 class RadarFrame:
    
-    def __init__(self, data, status=-1):
-            # data is a dictionary mapping keys to  lists 
-            # ex. {'sensorId': [1,2], 'x': [1,2], 'y': [1,2], 'z': [1,2], 'timestamp': [1,2]}
-            try:
-                 if isinstance(data, dict) and all(isinstance(val, list) for val in data.values()) and len(data) == 5:
-                    self.data = data
-            except:
-                raise TypeError("Error in RadarFrame instantiation: data must be a dictionary mapping keys to lists")
-            self.isStatic = status # -1 default, 1 static, 0 not static. checked by static points class
-            self.points_list = [(point['x'], point['y'], status) for point in self.data] # [[(x,y,static),(...)]
-
-    # check if a specified sensor is empty      
-    def is_empty(self, sensor_id) -> bool:  
-        s_id = 0 if sensor_id == 1 else 1 # get index of sensor id in self.data
-        return len(self.data[s_id]) == 0
+    def __init__(self, data):
+            # data is a list of dictionaries
+            # Each instance variable is a list of values from the dictionary 
+            # Reconstructed as a dict: {'sensorId': [1,2], 'x': [1,2], 'y': [1,2], 'z': [1,2], 'timestamp': [1,2]}
+            self.sensorid = []
+            self.x = []
+            self.y = []
+            self.z = []
+            self.timestamp = []
+            self.isStatic = [] # -1 default, 1 static, 0 not static. checked by static points class
+            for item in data: 
+                self.sensorid.append(item['sensorId'])
+                self.x.append(item['x'])
+                self.y.append(item['y'])
+                self.z.append(item['z'])
+                self.timestamp.append(item['timestamp'])
+                self.isStatic.append(-1)
     
+    def __str__(self): 
+        return f"""
+        RadarFrame object with: sensorid: {self.sensorid}, \n
+        x: {self.x}, \n
+        y: {self.y}, \n
+        z: {self.z}, \n
+        timestamp: {self.timestamp}, \n
+        is static: {self.isStatic}\n
+        """
+   
+    # check if a specified sensor is empty      
+    def is_empty(self, sensor_id=None) -> bool:  
+         # if sensor id is not passed in, check all sensors
+        if sensor_id == None:
+            return len(self.sensorid) == 0
+        else:
+        # if argument specifies sensor id, check data within that sensor id
+            for id in self.sensorid: # check if sensor id is in list of sensor ids
+                if id == sensor_id:
+                    return False # sensor id is not empty
+        return True # id not found, sensor is empty
+
     # a getter for points list to be used for display
     def get_points_for_display(self, sensor_id) -> list:
-        return self.points_list
-        
-
+        points_list = []
+        for i, id in enumerate(self.sensorid):
+            if id == sensor_id:
+                points_list.append((self.x[i], self.y[i], self.isStatic[i]))
+        return points_list
