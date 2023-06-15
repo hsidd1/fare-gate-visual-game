452e1fe7d6556743bb0df63e075d20ef650a1946
hsidd1 <hamzasiddiqui247@gmail.com>
2023-06-14 20:21:18 -0400

    update: next frame method updated with added vars and slight modifications in __str__ and logic

--- Radar_data.py
+++ Radar_data.py
@@ -16,6 +16,11 @@ class RadarData:
             self.z.append(item['z'])
             self.timestamp.append(item['timestamp'])
         self.__time_elapsed = 0
+        self.__initial_timestamp = None # set in set_initial_timestamp() to avoid overwriting after deletions in take_next_frame()
+    
+    def set_initial_timestamp(self):
+        if self.__initial_timestamp is None:
+            self.__initial_timestamp = self.timestamp[0]
 
     def get_num_sensors(self) -> int:
         has_sensor_1 = 1 in self.sensorid
@@ -34,7 +39,7 @@ class RadarData:
         return f"""
         RadarData object with: {self.get_num_sensors()} sensors, \n
         {len(self.x)} points, \n
-        and {self.timestamp[-1] - self.timestamp[0]} milliseconds of data \n
+        and {0 if not self.timestamp else self.timestamp[-1] - self.timestamp[0]} milliseconds of data \n
         """
         
     def has_data(self) -> bool:
@@ -42,10 +47,20 @@ class RadarData:
     
     # returns radar frame object for a specified interval
     def take_next_frame(self, interval) -> RadarFrame:
+        self.set_initial_timestamp() # very first timestamp in data
+        frame_last_ts = self.__initial_timestamp + self.__time_elapsed + interval
         self.__time_elapsed += interval
-        frame_first_ts = self.timestamp[0]
-        frame_last_ts = frame_first_ts + interval
-        frame_last_ts_index = self.timestamp.index(frame_last_ts)
+
+        frame_last_ts_index = None
+        for i, ts in enumerate(self.timestamp):
+            if ts <= frame_last_ts:
+                frame_last_ts_index = i + 1
+            else:
+                break
+        
+        if frame_last_ts_index is None:
+            return RadarFrame([])
+       
         extracted_data = []
         for i in range(frame_last_ts_index):
             extracted_data.append({
@@ -55,10 +70,9 @@ class RadarData:
                      'z': self.z[i],
                      'timestamp': self.timestamp[i]
                  })
-        for i in range(frame_last_ts_index):
-            del self.sensorid[0]
-            del self.x[0]
-            del self.y[0]
-            del self.z[0]
-            del self.timestamp[0]
+        del self.sensorid[:frame_last_ts_index]
+        del self.x[:frame_last_ts_index]
+        del self.y[:frame_last_ts_index]
+        del self.z[:frame_last_ts_index]
+        del self.timestamp[:frame_last_ts_index]
         return RadarFrame(extracted_data)
