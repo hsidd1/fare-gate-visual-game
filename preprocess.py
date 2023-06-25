13b3e7e50bd5185ca019f3da9a760b20a0294a6a
Elliot <hfyxin@gmail.com>
2023-06-25 14:48:56 -0400

    Code formatting

--- preprocess.py
+++ preprocess.py
@@ -1,6 +1,7 @@
 import numpy as np
 from radar_points import RadarData
 
+
 # for entry sensor
 def calc_rot_matrix(alpha, beta):
     """alpha is the angle along z axis - yaw
@@ -9,62 +10,43 @@ def calc_rot_matrix(alpha, beta):
     all angles are in degrees and counter.
     Rototation matrix is calculated in the order of z -> x -> y
     """
-    rotz = np.zeros((3,3))
-    rotz[0,0] = np.cos(np.radians(alpha))
-    rotz[0,1] = -np.sin(np.radians(alpha))
-    rotz[1,0] = np.sin(np.radians(alpha))
-    rotz[1,1] = np.cos(np.radians(alpha))
-    rotz[2,2] = 1
-    rotx = np.zeros((3,3))
-    rotx[0,0] = 1
-    rotx[1,1] = np.cos(np.radians(beta))
-    rotx[1,2] = -np.sin(np.radians(beta))
-    rotx[2,1] = np.sin(np.radians(beta))
-    rotx[2,2] = np.cos(np.radians(beta))
+    rotz = np.zeros((3, 3))
+    rotz[0, 0] = np.cos(np.radians(alpha))
+    rotz[0, 1] = -np.sin(np.radians(alpha))
+    rotz[1, 0] = np.sin(np.radians(alpha))
+    rotz[1, 1] = np.cos(np.radians(alpha))
+    rotz[2, 2] = 1
+    rotx = np.zeros((3, 3))
+    rotx[0, 0] = 1
+    rotx[1, 1] = np.cos(np.radians(beta))
+    rotx[1, 2] = -np.sin(np.radians(beta))
+    rotx[2, 1] = np.sin(np.radians(beta))
+    rotx[2, 2] = np.cos(np.radians(beta))
     return rotz, rotx
+
 
 def rot_mtx_entry(alpha, beta):
     return calc_rot_matrix(alpha, beta)
 
+
 def rot_mtx_exit(alpha, beta):
     return calc_rot_matrix(alpha + 180, beta)
 
+
 def load_data_sensorhost(data: dict) -> RadarData:
     radar_points = []
-    for i in data['frames']:
-
-        num_ob = i['sensorMessage']['metadata']['numOfDetectedObjects']
-
-        # timestamp = ((i['sensorMessage']['metadata']['timeStamp']))
-        timestamp = ((i['timestamp']))
-        # print('WORLD TIME' + str(timestamp))
+    for item in data["frames"]:
+        num_ob = item["sensorMessage"]["metadata"]["numOfDetectedObjects"]
+        detected_points = item["sensorMessage"]["object"]["detectedPoints"]
+        timestamp = item["timestamp"]  # world time?
 
         for j in range(num_ob):
             s = dict()
-            s['sensorId'] = (i['sensorMessage']['object']['detectedPoints'][j]['sensorId'])
-            s['x'] = i['sensorMessage']['object']['detectedPoints'][j]['x']
-            s['y'] = i['sensorMessage']['object']['detectedPoints'][j]['y']
-            s['z'] = i['sensorMessage']['object']['detectedPoints'][j]['z']  # Flattened so it should always return be 0
-            s['timestamp'] = timestamp
-            # print('TIMESTAMP' + str(timestamp))
-            #
-            raw_coords = np.asmatrix(([s['x']], [s['y']], [s['z']])) * 10  #  cm to mm
-            if s['sensorId'] == 1:
-                # entry sensor
-                # transformed_coords = np.matmul(s1_rotz, np.matmul(s1_rotx, raw_coords))
-                # transformed_coords += np.array([[offsetx], [-offsety], [offsetz]])
-                transformed_coords = raw_coords
-            elif s['sensorId'] == 2:
-                # transformed_coords = np.matmul(s2_rotz, np.matmul(s2_rotx, raw_coords))
-                # transformed_coords += np.array([[-offsetx], [offsety], [offsetz]])
-                transformed_coords = raw_coords
-            else:
-                print('Sensor ID not supported')
-                raise
+            s["sensorId"] = detected_points[j]["sensorId"]
+            s["x"] = detected_points[j]["x"] * 10  # converting to mm
+            s["y"] = detected_points[j]["y"] * 10
+            s["z"] = detected_points[j]["z"] * 10
+            s["timestamp"] = timestamp
 
-            s['x'] = float(transformed_coords[0])
-            s['y'] = float(transformed_coords[1])
-            s['z'] = float(transformed_coords[2])
-            
             radar_points.append(s)
     return RadarData(radar_points)
--- rta2_cv2visual.py
+++ rta2_cv2visual.py
@@ -1,17 +1,16 @@
 import cv2
 import json
-import numpy as np
 import yaml
 from radar_points import RadarData, StaticPoints
 from preprocess import load_data_sensorhost, rot_mtx_entry, rot_mtx_exit
 
 # ------------------ DATA PREPROCESS ------------------ #
 # load configuration
-with open('config.yaml', 'r') as file:
+with open("config.yaml", "r") as file:
     config = yaml.safe_load(file)
 
 # load json data
-radar_data_file = config['Files']['radar_data_file']
+radar_data_file = config["Files"]["radar_data_file"]
 with open(radar_data_file) as json_file:
     data = json.load(json_file)
 
@@ -20,11 +19,12 @@ radar_data = load_data_sensorhost(data)  # Original coordinates
 print(f"Radar data loaded.\n{radar_data}\n")
 
 # Apply transformation
-alpha = config['SensorAngles']['alpha']
-beta = config['SensorAngles']['beta']
-offsetx = config['SensorOffsets']['offsetx']   # distance of sensor from gate centre, positive in mm
-offsety = config['SensorOffsets']['offsety']
-offsetz = config['SensorOffsets']['offsetz']
+alpha = config["SensorAngles"]["alpha"]
+beta = config["SensorAngles"]["beta"]
+# distance of sensor from gate centre, positive in mm
+offsetx = config["SensorOffsets"]["offsetx"]
+offsety = config["SensorOffsets"]["offsety"]
+offsetz = config["SensorOffsets"]["offsetz"]
 print(f"alpha: {alpha}")
 print(f"beta: {beta}")
 print(f"offsetx: {offsetx}")
@@ -33,16 +33,18 @@ print(f"offsetz: {offsetz}")
 s1_rotz, s1_rotx = rot_mtx_entry(alpha, beta)
 s2_rotz, s2_rotx = rot_mtx_exit(alpha, beta)
 
-radar_data.transform_coord(s1_rotz, s1_rotx, s2_rotz, s2_rotx, offsetx, offsety, offsetz)
+radar_data.transform_coord(
+    s1_rotz, s1_rotx, s2_rotz, s2_rotx, offsetx, offsety, offsetz
+)
 print(f"Radar data transformed.\n{radar_data}\n")
 
 # ------------------ VISUALIZATION PARAMS ------------------ #
-rad_cam_offset = config['rad_cam_offset']
-scalemm2px = config['scalemm2px']
-wait_ms = config['wait_ms']
-slider_xoffset = config['TrackbarDefaults']['slider_xoffset']
-slider_yoffset = config['TrackbarDefaults']['slider_yoffset']
-xy_trackbar_scale = config['TrackbarDefaults']['xy_trackbar_scale']
+rad_cam_offset = config["rad_cam_offset"]
+scalemm2px = config["scalemm2px"]
+wait_ms = config["wait_ms"]
+slider_xoffset = config["TrackbarDefaults"]["slider_xoffset"]
+slider_yoffset = config["TrackbarDefaults"]["slider_yoffset"]
+xy_trackbar_scale = config["TrackbarDefaults"]["xy_trackbar_scale"]
 
 print(f"rad_cam_offset: {rad_cam_offset}")
 print(f"scalemm2px: {scalemm2px}")
@@ -53,30 +55,35 @@ print(f"xy_trackbar_scale: {xy_trackbar_scale}")
 
 # ------------------ CV2 SUPPORT FUNCTIONS ------------------ #
 
-# BGR colours for drawing points on frame (OpenCV) 
+# BGR colours for drawing points on frame (OpenCV)
 GREEN = (0, 255, 0)
 YELLOW = (0, 255, 255)
 BLUE = (255, 0, 0)
+
 
 def washout(color, factor=0.2):
     # create washed out color
     return (int(color[0] * factor), int(color[1] * factor), int(color[2] * factor))
 
-#Trackbar configuration
+
+# Trackbar configuration
 def x_trackbar_callback(x):
-    # updates global offsets by trackbar value 
+    # updates global offsets by trackbar value
     global slider_xoffset
-    slider_xoffset = cv2.getTrackbarPos('x offset', 'Radar Visualization')
+    slider_xoffset = cv2.getTrackbarPos("x offset", "Radar Visualization")
+
 
 def y_trackbar_callback(x):
-    # updates global offsets by trackbar value 
-    global  slider_yoffset
-    slider_yoffset = cv2.getTrackbarPos('y offset', 'Radar Visualization')
+    # updates global offsets by trackbar value
+    global slider_yoffset
+    slider_yoffset = cv2.getTrackbarPos("y offset", "Radar Visualization")
+
 
 def scale_callback(x):
-    # multiplies x and y by scale value from trackbar 
+    # multiplies x and y by scale value from trackbar
     global xy_trackbar_scale
-    xy_trackbar_scale = cv2.getTrackbarPos('scale %', 'Radar Visualization') / 100
+    xy_trackbar_scale = cv2.getTrackbarPos("scale %", "Radar Visualization") / 100
+
 
 def draw_radar_points(points, sensor_id):
     if sensor_id == 1:
@@ -86,28 +93,30 @@ def draw_radar_points(points, sensor_id):
     else:
         raise
     for coord in points:
-        x = int((coord[0] + offsetx) * scalemm2px)  
-        y = int((-coord[1] + offsety) * scalemm2px)   # y axis is flipped 
-        z = int(coord[2] * scalemm2px)   # z is not used
+        x = int((coord[0] + offsetx) * scalemm2px)
+        y = int((-coord[1] + offsety) * scalemm2px)  # y axis is flipped
+        z = int(coord[2] * scalemm2px)  # z is not used
         static = coord[3]
-        
+
         # xy modifications from trackbar controls
         x = int(x * xy_trackbar_scale)
         y = int(y * xy_trackbar_scale)
         x += slider_xoffset
-        y += slider_yoffset 
+        y += slider_yoffset
         if static:
-            cv2.circle(frame, (x,y), 4, washout(color), -1)
+            cv2.circle(frame, (x, y), 4, washout(color), -1)
         else:
-            cv2.circle(frame, (x,y), 4, color, -1)
-            
-# draw gate at top left of window, with width and height of gate. Scale to match gate location with trackbar 
-def draw_gate_topleft(): 
+            cv2.circle(frame, (x, y), 4, color, -1)
+
+
+# draw gate at top left of window, with width and height of gate. Scale to match gate location with trackbar
+def draw_gate_topleft():
     # initially at top left corner
-    start_x = 0 
-    start_y = 0 
+    start_x = 0
+    start_y = 0
     # modify start_x and start_y based on trackbar values
-    start_x, start_y = int(start_x * xy_trackbar_scale), int(start_y * xy_trackbar_scale)
+    start_x = int(start_x * xy_trackbar_scale)
+    start_y = int(start_y * xy_trackbar_scale)
     start_x += slider_xoffset
     start_y += slider_yoffset
     rect_start = (start_x, start_y)
@@ -121,6 +130,7 @@ def draw_gate_topleft():
     rect_end = (end_x, end_y)
     cv2.rectangle(frame, rect_start, rect_end, BLUE, 2)
 
+
 def display_video_info(radar_frame: RadarData, width, height):
     """Display video info on frame. width and height are the dimensions of the window."""
     # TODO: this part is broken. Need to revise.
@@ -133,13 +143,13 @@ def display_video_info(radar_frame: RadarData, width, height):
     text_str = f"Curr frame ts:{(t_rad-T_RAD_BEGIN)/1000:.3f}   Replay {1:.1f}x"
     cv2.putText(frame, text_str, (10, height-40), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 2)
     # find timestamps of sensors
-    sensor1_timestamps = list(set([coord['timestamp'] for coord in s1_pts]))
+    sensor1_timestamps = list(set([coord["timestamp"] for coord in s1_pts]))
     text_str = f"s_entry ts:"
     for i, timestamp in enumerate(sensor1_timestamps):
         text_str += f" s1[{i}]: {(timestamp-TS_OFFSET)/1000:.3f}"
     cv2.putText(frame, text_str, (10, height-100), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 2)
     # find timestamps of sensors
-    sensor2_timestamps = list(set([coord['timestamp'] for coord in s2_pts]))
+    sensor2_timestamps = list(set([coord["timestamp"] for coord in s2_pts]))
     text_str = f"s_exit ts: "
     for i, timestamp in enumerate(sensor2_timestamps):
         text_str += f" s2[{i}]: {(timestamp-TS_OFFSET)/1000:.3f}"
@@ -150,22 +160,22 @@ def display_video_info(radar_frame: RadarData, width, height):
 
 # ------------------ VISUALIZATION ------------------ #
 
-# radar camera synchronization
-rad_cam_offset = rad_cam_offset - rad_cam_offset % (100)  # make sure it's multiples of video frame interval
-print(f"Radar is set to be ahead of video by {rad_cam_offset}ms.")
-# t_vid = cap.get(cv2.CAP_PROP_POS_MSEC)   # video's timestamp, in ms
-
 # video frame buffer
-frame_prev = None
-video_file = config['Files']['video_file']
+video_file = config["Files"]["video_file"]
 cap = cv2.VideoCapture(video_file)
 num_frames = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
 print(f"Number of frames: {num_frames}")
 
-cv2.namedWindow('Radar Visualization')
-cv2.createTrackbar('x offset', 'Radar Visualization', slider_xoffset, 2000, x_trackbar_callback)
-cv2.createTrackbar('y offset', 'Radar Visualization', slider_yoffset, 2000, y_trackbar_callback)
-cv2.createTrackbar('scale %', 'Radar Visualization', int(xy_trackbar_scale*100), 200, scale_callback) # *100 and /100 to account for floating point usuability to downscale
+cv2.namedWindow("Radar Visualization")
+cv2.createTrackbar(
+    "x offset", "Radar Visualization", slider_xoffset, 2000, x_trackbar_callback
+)
+cv2.createTrackbar(
+    "y offset", "Radar Visualization", slider_yoffset, 2000, y_trackbar_callback
+)
+cv2.createTrackbar(
+    "scale %", "Radar Visualization", int(xy_trackbar_scale * 100), 200, scale_callback
+)  # *100 and /100 to account for floating point usuability to downscale
 
 # static points buffer
 s1_static = StaticPoints(cnt_thres=5)
@@ -176,11 +186,17 @@ s1_display_points_prev = []
 s2_display_points_prev = []
 
 # frame interval, set to the same as video
-incr = 1000/30   # frame ts increment, in ms
+incr = 1000 / 30  # frame ts increment, in ms
+
+# radar camera synchronization
+rad_cam_offset = rad_cam_offset - rad_cam_offset % (
+    incr
+)  # make sure it's multiples of video frame interval
+print(f"Radar is set to be ahead of video by {rad_cam_offset:.1f}ms.")
 
 # Prepare for main loop: remove radar points, if radar is ahead
 all_increments = 0
-ts_start = radar_data.ts[0] # initial timestamp of radar points at start of program
+ts_start = radar_data.ts[0]  # initial timestamp of radar points at start of program
 if round(rad_cam_offset) > 0:
     print("rad_cam_offset is set positive, removing radar points while waiting for video.")
 while round(rad_cam_offset) > 0:
@@ -194,11 +210,11 @@ while round(rad_cam_offset) > 0:
         radar_data.ts.pop(0)
     rad_cam_offset -= incr
     # print(f"rad_cam_offset is now: {0 if rad_cam_offset < 1 else rad_cam_offset}")
-    t_rad = radar_data.ts[0]   # update t_rad to the timestamp of the first point in the frame
+    t_rad = radar_data.ts[0]  # timestamp of the first point in frame
 
 # Prepare for main loop: skip video frames, if video is ahead
 if round(rad_cam_offset) < 0:
-    print("rad_cam_offset is set negative, waiting radar points while video is playing.") 
+    print("rad_cam_offset is set negative, waiting radar points while playing video.")
 while round(rad_cam_offset) < 0:
     rad_cam_offset += incr
     ret, frame = cap.read()
@@ -212,7 +228,7 @@ while True:
     if not ret:
         break
     height, width = frame.shape[:2]
-    frame = cv2.resize(frame, (round(width), round(height)))   # reduce frame size
+    frame = cv2.resize(frame, (round(width), round(height)))  # reduce frame size
     frame = cv2.rotate(frame, cv2.ROTATE_90_CLOCKWISE)
     height, width = frame.shape[:2]
 
@@ -252,34 +268,35 @@ while True:
     display_video_info(radar_frame, width, height) 
     
     # after drawing points on frames, imshow the frames
-    cv2.imshow('Radar Visualization', frame)
+    cv2.imshow("Radar Visualization", frame)
 
     # Key controls
-    cv2.putText(frame, "Controls - 'q': quit  'p': pause", (width-175, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.35, (0, 0, 150), 1)
     key = cv2.waitKey(wait_ms) & 0xFF
-    if key == ord('q'): # quit program if 'q' is pressed 
+    if key == ord("q"):  # quit program if 'q' is pressed
         break
-    elif key == ord('p'): # pause/unpause program if 'p' is pressed
+    elif key == ord("p"):  # pause/unpause program if 'p' is pressed
         cv2.waitKey(0)
 
 cap.release()
 cv2.destroyAllWindows()
 
+# ------------------ SAVE CONFIG ------------------ #
 def yaml_update():
     while True:
         choice = input("Update final trackbar values in yaml? (y/n): ").lower()
-        if choice == 'y':
-            config['TrackbarDefaults']['slider_xoffset'] = slider_xoffset
-            config['TrackbarDefaults']['slider_yoffset'] = slider_yoffset
-            config['TrackbarDefaults']['xy_trackbar_scale'] = xy_trackbar_scale
-            with open('config.yaml', 'w') as file:
+        if choice == "y":
+            config["TrackbarDefaults"]["slider_xoffset"] = slider_xoffset
+            config["TrackbarDefaults"]["slider_yoffset"] = slider_yoffset
+            config["TrackbarDefaults"]["xy_trackbar_scale"] = xy_trackbar_scale
+            with open("config.yaml", "w") as file:
                 yaml.dump(config, file)
             print("Trackbar values updated in yaml.")
             break
-        elif choice == 'n':
+        elif choice == "n":
             print("Trackbar values not updated in yaml.")
             break
         else:
             print("Invalid input. Please enter 'y' or 'n'.")
+
 
 # yaml_update()
