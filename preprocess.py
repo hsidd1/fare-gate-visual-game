7e366afd50e0290441b536dede3104874ea862b4
hsidd1 <hamzasiddiqui247@gmail.com>
2023-08-30 11:33:42 -0400

    add mqtt format loading

--- preprocess.py
+++ preprocess.py
@@ -35,6 +35,7 @@ def rot_mtx_exit(alpha, beta):
 
 
 def load_data_sensorhost(data: json) -> RadarData:
+    # load data in elementary version sensorhost format (Control_test1.json)
     radar_points = []
     for item in data["frames"]:
         num_ob = item["sensorMessage"]["metadata"]["numOfDetectedObjects"]
@@ -53,11 +54,13 @@ def load_data_sensorhost(data: json) -> RadarData:
     return RadarData(radar_points)
 
 def load_data_tlv(data: json) -> RadarData:
+    # load data in radar-only TI output format (Test2_tlv_data_log.json)
     radar_points = []
     for item in data:
         for j in range(len(item["x"])):
             s = dict()
             s["sensorId"] = item["Sensor_id"]
+            # uses TLV for visualization
             s["TLV_type"] = item["TLV_type"]
             s["x"] = item["x"][j] * 1000  # converting to mm
             s["y"] = item["y"][j] * 1000
@@ -70,4 +73,32 @@ def load_data_tlv(data: json) -> RadarData:
             s["timestamp"] = milliseconds
             radar_points.append(s)
     # return data object specifying format for visualization
+    return RadarData(radar_points, isTLVformat=True)
+
+def load_data_mqtt(data: json) -> RadarData:
+    # load data in mqtt save data format (radcamlog.json)
+    radar_points = []
+    for item in data:
+        if item["topic"] == "data/liveradar":
+            for j in range(len(item["radar_payload"]["x"])):
+                s = dict()
+                s["sensorId"] = item["radar_payload"]["Sensor_id"]
+                # uses TLV for visualization
+                s["TLV_type"] = item["radar_payload"]["TLV_type"]
+                # data is in m from origin of radar
+                s["x"] = item["radar_payload"]["x"][j] * 1000  # converting to mm
+                s["y"] = item["radar_payload"]["y"][j] * 1000
+                s["z"] = item["radar_payload"]["z"][j] * 1000
+
+                # timestamp from receiver on subscribe
+                # time_str = item["sub_ts"]
+                # time_obj = datetime.datetime.strptime(time_str, "%H:%M:%S.%f")
+                # convert datetime to milliseconds
+                # milliseconds = int(time_obj.hour * 3600 + time_obj.minute * 60 + time_obj.second) * 1000 + time_obj.microsecond // 1000
+                # s["timestamp"] = milliseconds
+
+                # alternatively, use timestamp from radar
+                s["timestamp"] = int(item["radar_payload"]["time"].replace(":", "").replace(".", "")[:-3])
+                
+                radar_points.append(s)
     return RadarData(radar_points, isTLVformat=True)
