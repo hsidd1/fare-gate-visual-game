2f47f07a61841f8608cd6cbf2d03f12cda8c752b
hsidd1 <hamzasiddiqui247@gmail.com>
2023-08-30 11:04:42 -0400

    generalize test function

--- tests/test_preprocess.py
+++ tests/test_preprocess.py
@@ -2,14 +2,25 @@ import json
 import sys
 sys.path.append("./")
 from radar_points import * 
-# validation for test2 output
-def test_t2load_tlv():
-    import preprocess
-    with open("data/Test2_tlv_data_log.json", "r") as f:
+import preprocess
+
+"""
+visually view data per frame at specified interval for 
+different loading functions
+"""
+def test_load(filename: str, func: str, interval=30) -> None:
+    with open(filename, "r") as f:
         data = json.load(f)
+    if func == "load_data_tlv":
         radar_data = preprocess.load_data_tlv(data)
-        print(radar_data)
-    interval = 30
+    elif func == "load_data_mqtt":
+        radar_data = preprocess.load_data_mqtt(data)
+    elif func == "load_data_sensorhost":
+        radar_data = preprocess.load_data_sensorhost(data)
+    else:
+        raise ValueError("Invalid function name")
+    print(f"loading data with {func}...")
+    print(radar_data)
     while radar_data.has_data():
         radar_frame = radar_data.take_next_frame(interval)
         print(radar_frame)
@@ -18,7 +29,7 @@ def test_t2load_tlv():
         print(radar_data)
 
 def main():
-    test_t2load_tlv()
-
+    test_load(filename="data/Test2_tlv_data_log.json", func="load_data_tlv")
+    test_load(filename="data/Control_test1.json", func="load_data_sensorhost")
 if __name__ == "__main__":
     main()
